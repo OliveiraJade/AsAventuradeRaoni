8bd03b283eb1fd24541c2fe9514de2bfdc946a94
oliveirajade <oliveiradepaulajaqueline@gmail.com>
2019-10-02 22:16:26 -0300

    Atualização

--- As Aventura de Raoni/Assets/Scripts/Personagem.cs
+++ As Aventura de Raoni/Assets/Scripts/Personagem.cs
@@ -7,6 +7,13 @@ public class Personagem : MonoBehaviour
     public float velocidade = 4;
     public bool olhandoParaDireita = true;
 
+    //pulo
+    bool noChao = false;
+    float chaoCheckRaio = 0.2f;
+    public int forcaPulo = 100;
+    public Transform chaoCheck;
+    public LayerMask OQueEChao;
+
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -18,12 +25,37 @@ public class Personagem : MonoBehaviour
     // Update is called once per frame
     void Mover()
     {
+        //pulo
+        noChao = Physics2D.OverlapCircle(chaoCheck.position, chaoCheckRaio, OQueEChao);
+
+        // virar 
+
         float horizontalForceButton = Input.GetAxis ("Horizontal");
         rb.velocity = new Vector2 (horizontalForceButton * velocidade, rb.velocity.y);
+
+        //virar
+        if((horizontalForceButton > 0 && !olhandoParaDireita) || (horizontalForceButton < 0 && olhandoParaDireita))
+        {
+            Virar();
+        }
+    }
+
+    void Virar()
+    {
+        olhandoParaDireita = !olhandoParaDireita;
+        Vector3 myScale = transform.localScale;
+        myScale.x *= -1; //Atribuição para virar
+        transform.localScale = myScale;
     }
 
     void Update()
     {
-        Mover();
+       Mover();
+
+        //pulo
+        if(noChao && Input.GetButton ("Jump"))
+        {
+            GetComponent<Rigidbody2D>().AddForce(new Vector2(0, forcaPulo));
+        }
     }
 }
