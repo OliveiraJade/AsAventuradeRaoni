9f7ff8b637b5118b1c5fa3c2eca7fd9aee2b831a
oliveirajade <oliveiradepaulajaqueline@gmail.com>
2019-10-04 22:18:44 -0300

    Atualização

---
+++ As Aventura de Raoni/Assets/Scripts/Portas.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portas : MonoBehaviour
+{
+    private Jogo jogo;
+
+    void Start()
+    {
+        GameObject objetoJogo = GameObject.FindWithTag("JOGO");
+        jogo = objetoJogo.GetComponent<Jogo>();
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D colisor)
+    {
+        if (colisor.gameObject.tag == "Player")
+        {
+            SceneManager.LoadScene("Fase2");
+        }
+    }
+
+}
